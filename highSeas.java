eb6c6b3a579f6d4ed6d6c1ebf9caee243e1d9ebb
amogh240 <79285545+amogh240@users.noreply.github.com>
2023-10-24 17:18:27 -0500

    Add files via upload

---
+++ highSeas.java
@@ -0,0 +1,17 @@
+package Complete;
+import static java.lang.System.*;
+import java.util.*;
+import java.io.*;
+
+public class highSeas {
+    public static void main(String[] args) throws IOException {
+        out.println("     |    |    |   ");
+        out.println("    )_)  )_)  )_)");
+        out.println("   )___))___))___)\\");
+        out.println("  )____)____)_____)\\");
+        out.println("_____|____|____|____\\");
+        out.println("\\ @ @ @  SS HighSeas/");
+        out.println(" -------------------");
+        
+    }
+}
